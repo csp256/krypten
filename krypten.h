beaa85d6722257128428b04b725badf5d01f87df
Christopher Parker <csp256@users.noreply.github.com>
2020-03-24 02:42:15 -0700

    Create krypten.h

---
+++ krypten.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <array>
+#include <string>
+#include <vector>
+
+namespace krypten {
+
+    class Krypten {
+    public:
+        struct alignas(16) Block : std::array<uint32_t, 4> {};
+        struct alignas(16) Key : std::array<uint8_t, 32> {};
+
+        explicit 
+        Krypten(Key const& key);
+
+        static Key
+        random_key();
+
+        std::vector<Krypten::Block> 
+        encrypt(std::string const& data) const;
+        
+        std::string 
+        decrypt(std::vector<Krypten::Block> const& data) const;
+
+    private:
+        void 
+        increment_IV(Block& IV) const;
+
+        Block 
+        encrypt_block(Block const& DATA) const;
+
+        std::array<Block, 15> keys;
+    };
+
+}
